5e0d632f2a89cf640febc3b13c66b1dfaf0c5490
ChandanaMS <59191388+ChandanaMS-684@users.noreply.github.com>
2022-03-15 22:25:29 +0530

    Adding comments for readability

--- src/App.js
+++ src/App.js
@@ -1,80 +1,124 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import axios from 'axios';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
 import Table from "./components/Table";
-import { TextField, Typography } from '@mui/material';
-import Drawer from '@mui/material/Drawer';
-
+import { TextField, Typography } from "@mui/material";
+import Drawer from "@mui/material/Drawer";
 
 function App() {
+  /*Using React Hook for current state and changed state
+    for functional component*/
   const [loading, setLoading] = useState(true);
-  const [docs, setdocs] = useState([]);    //book data
+  const [docs, setdocs] = useState([]); //book data
   const [open, setOpen] = useState(false); //drawer
-  const [authorDetails, setAuthorDetails] = useState([]) //author-details 
+  const [authorDetails, setAuthorDetails] = useState([]); //author-details
   const fetchData = async (url) => {
     return await axios.get(url);
-  }
+  };
 
+  //Passing the public API consisting of book-data and fetching it by using axios
   useEffect(async () => {
-    const url = 'https://openlibrary.org/search.json?author=tolkien';
+    const url = "https://openlibrary.org/search.json?author=tolkien";
     const fetchResult = await axios.get(url);
-    setdocs(fetchResult?.data?.docs)
-    setLoading(false)
-  }, [])
+    setdocs(fetchResult?.data?.docs);
+    setLoading(false);
+  }, []);
 
+  //Search Book Functionality to search book by title in Search Bar
   const handleSearch = async (e) => {
     setLoading(true);
+    //condition to check if searched data is not empty
     if (e.target.value !== "") {
-      const res = docs.filter((item) => item.title.toLowerCase().includes(e.target.value.toLowerCase()))
-      setdocs(
-        res
-      )
+      //Filtering books using searched data
+      const res = docs.filter((item) =>
+        item.title.toLowerCase().includes(e.target.value.toLowerCase())
+      );
+      //setting the filtered data to res
+      setdocs(res);
+    } else {
+      const res = await fetchData(
+        "https://openlibrary.org/search.json?author=tolkien"
+      );
+      setdocs(res?.data?.docs);
     }
-    else {
-      const res = await fetchData('https://openlibrary.org/search.json?author=tolkien');
-      setdocs(res?.data?.docs)
-    }
-    setLoading(false)
-  }
+    setLoading(false);
+  };
 
+  /*Drawer functionality
+    -Initially drawer opening state is set to false
+    and setAuthorDetails is set to empty array*/
   const toggleDrawer = () => {
     setOpen(false);
-    setAuthorDetails([])
-  }
+    setAuthorDetails([]);
+  };
 
+  //When clicked on Author,the side drawer shows up with Author Details fetched from API
   const getAuthorDetails = async (author) => {
-    setOpen(true)
-    const res = await fetchData("https://openlibrary.org/search/authors.json?q=" + author);
+    setOpen(true);
+    const res = await fetchData(
+      "https://openlibrary.org/search/authors.json?q=" + author
+    );
     setAuthorDetails(res?.data?.docs);
-  }
+  };
+  // Styling the header and search bar using Typography
   return (
     <div>
-      <div align='center'>
-        <Typography color="darkslateblue" padding="10px" align="center" variant='h3'>BOOK LIBRARY</Typography>
-        <TextField type="search" variant='outlined' label="Search by Title..." onChange={(e) => {
-          handleSearch(e)
-        }} />
+      <div align="center">
+        <Typography
+          color="darkslateblue"
+          padding="10px"
+          align="center"
+          variant="h3"
+        >
+          BOOK LIBRARY
+        </Typography>
+        <TextField
+          type="search"
+          variant="outlined"
+          label="Search by Title..."
+          onChange={(e) => {
+            handleSearch(e);
+          }}
+        />
       </div>
 
-      {loading ? <h2 align='center'> Loading Please wait .... </h2> : <Table filteredData={docs} getAuthorDetails={getAuthorDetails} />}
-      {
-        open && (<Drawer
+      {loading ? (
+        <h2 align="center"> Loading Please wait .... </h2> //Loading is displayed untill the data is fetched
+      ) : (
+        <Table filteredData={docs} getAuthorDetails={getAuthorDetails} />
+      )}
+      {open && (
+        <Drawer //anchor defines the side of the Drawer i.e.,right
           anchor={"right"}
           open={open}
-          onClose={() => { toggleDrawer() }}
+          onClose={() => {
+            toggleDrawer();
+          }}
         >
-          {/* <h1>AUTHOR DETAILS</h1> */}
-          <Typography color="darkcyan" variant='h4' align="center">AUTHOR DETAILS</Typography>
-          {
-            authorDetails.length > 0 ? <>
-              <Typography variant='h5'>Name: {authorDetails[0]?.name}</Typography>
-              <Typography variant='h5'>Year-of-Birth: {authorDetails[0]?.birth_date}</Typography>
-              <Typography variant='h5'>Year-of-Death: {authorDetails[0]?.death_date}</Typography>
-              <Typography variant='h5'>Top-Work: {authorDetails[0]?.top_work}</Typography>
-            </> : <Typography>Getting Author Details ... </Typography>
-          }
-        </Drawer>)
-      }
+          <Typography color="darkcyan" variant="h4" align="center">
+            AUTHOR DETAILS
+          </Typography>{" "}
+          {/*Displaying Author Details in Drawer,styling using Typography */}
+          {authorDetails.length > 0 ? (
+            <>
+              <Typography variant="h5">
+                Name: {authorDetails[0]?.name}
+              </Typography>
+              <Typography variant="h5">
+                Year-of-Birth: {authorDetails[0]?.birth_date}
+              </Typography>
+              <Typography variant="h5">
+                Year-of-Death: {authorDetails[0]?.death_date}
+              </Typography>
+              <Typography variant="h5">
+                Top-Work: {authorDetails[0]?.top_work}
+              </Typography>
+            </>
+          ) : (
+            <Typography>Getting Author Details ... </Typography>
+          )}
+        </Drawer>
+      )}
     </div>
   );
 }
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,181 +1,195 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import { useTheme } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableFooter from '@mui/material/TableFooter';
-import TablePagination from '@mui/material/TablePagination';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import IconButton from '@mui/material/IconButton';
-import FirstPageIcon from '@mui/icons-material/FirstPage';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import LastPageIcon from '@mui/icons-material/LastPage';
-import { Button, TableHead } from '@mui/material';
+import * as React from "react";
+import PropTypes from "prop-types";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import LastPageIcon from "@mui/icons-material/LastPage";
+import { Button, TableHead } from "@mui/material";
 
-
+//Table column names of Book Details
 const columns = [
-    { field: 'title', headerName: 'Book-Title' },
-    { field: 'first_publish_year', headerName: 'Year' },
-    { field: 'author_name', headerName: 'Author' }
-]
+  { field: "title", headerName: "Book-Title" },
+  { field: "first_publish_year", headerName: "Year" },
+  { field: "author_name", headerName: "Author" }
+];
 
+//Book Pagination
 function TablePaginationActions(props) {
-    const theme = useTheme();
-    const { count, page, rowsPerPage, onPageChange } = props;
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
 
-    const handleFirstPageButtonClick = (event) => {
-        onPageChange(event, 0);
-    };
+  // Declaring Button Click events for Pages in Pagination
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
 
-    const handleBackButtonClick = (event) => {
-        onPageChange(event, page - 1);
-    };
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
 
-    const handleNextButtonClick = (event) => {
-        onPageChange(event, page + 1);
-    };
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
 
-    const handleLastPageButtonClick = (event) => {
-        onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-    };
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
 
-    return (
-        <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-            <IconButton
-                onClick={handleFirstPageButtonClick}
-                disabled={page === 0}
-                aria-label="first page"
-            >
-                {theme.direction === 'rtl' ? <LastPageIcon /> : <FirstPageIcon />}
-            </IconButton>
-            <IconButton
-                onClick={handleBackButtonClick}
-                disabled={page === 0}
-                aria-label="previous page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-            </IconButton>
-            <IconButton
-                onClick={handleNextButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="next page"
-            >
-                {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-            </IconButton>
-            <IconButton
-                onClick={handleLastPageButtonClick}
-                disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-                aria-label="last page"
-            >
-                {theme.direction === 'rtl' ? <FirstPageIcon /> : <LastPageIcon />}
-            </IconButton>
-        </Box>
-    );
+  // returns the action of onClick event of the button
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
 }
 
 TablePaginationActions.propTypes = {
-    count: PropTypes.number.isRequired,
-    onPageChange: PropTypes.func.isRequired,
-    page: PropTypes.number.isRequired,
-    rowsPerPage: PropTypes.number.isRequired,
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired
 };
 
+/*Passing the BookData,AuthorDetailsData to a BookTable function 
+  that returns the data in the table and authordetails in drawer*/
 export default function BookTable({ filteredData, getAuthorDetails }) {
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
 
-    // Avoid a layout jump when reaching the last page with empty rows.
-    const emptyRows =
-        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - filteredData.length) : 0;
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - filteredData.length) : 0;
 
-    const handleChangePage = (event, newPage) => {
-        setPage(newPage);
-    };
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
 
-    const handleChangeRowsPerPage = (event) => {
-        setRowsPerPage(parseInt(event.target.value, 10));
-        setPage(0);
-    };
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
-    const handleAuthor = (author) => {
-        getAuthorDetails(author)
-    }
+  const handleAuthor = (author) => {
+    getAuthorDetails(author);
+  };
 
-    return (
-        <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 500 }}>
-                <TableHead>
-                    <TableRow>
-                        {
-                            columns && columns.map((column) => {
-                                return (
-                                    <TableCell>
-                                        {column.headerName}
-                                    </TableCell>
-                                )
-                            })
-                        }
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {(rowsPerPage > 0
-                        ? filteredData.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                        : filteredData
-                    ).map((row) => (
-                        <TableRow key={row.name}>
-                            <TableCell component="th" scope="row">
-                                {row.title}
-                            </TableCell>
-                            <TableCell >
-                                {row.first_publish_year}
-                            </TableCell>
-                            <TableCell >
-                                {
-                                    row.author_name.map((author) => {
-                                        return (
-                                            <>
-                                                <Button onClick={() => { handleAuthor(author) }} >{author}</Button>
-                                            </>
-                                        )
-                                    })
-                                }
+  //Book Details is displayed in Table format
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 500 }}>
+        <TableHead>
+          <TableRow>
+            {columns &&
+              columns.map((column) => {
+                return <TableCell>{column.headerName}</TableCell>; //Returns Column Names
+              })}
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {(rowsPerPage > 0
+            ? filteredData.slice(
+                page * rowsPerPage,
+                page * rowsPerPage + rowsPerPage
+              )
+            : filteredData
+          ) //Displaying rows with BookData in TABLE
+            .map((row) => (
+              <TableRow key={row.name}>
+                <TableCell component="th" scope="row">
+                  {row.title}
+                </TableCell>
+                <TableCell>{row.first_publish_year}</TableCell>
+                <TableCell>
+                  {row.author_name.map((author) => {
+                    return (
+                      <>
+                        <Button
+                          onClick={() => {
+                            handleAuthor(author);
+                          }}
+                        >
+                          {author}
+                        </Button>
+                      </>
+                    );
+                  })}
+                </TableCell>
+              </TableRow>
+            ))}
 
-                            </TableCell>
-                        </TableRow>
-                    ))}
-
-                    {emptyRows > 0 && (
-                        <TableRow style={{ height: 53 * emptyRows }}>
-                            <TableCell colSpan={6} />
-                        </TableRow>
-                    )}
-                </TableBody>
-                <TableFooter>
-                    <TableRow>
-                        <TablePagination
-                            rowsPerPageOptions={[5, 10, 20, { label: 'All', value: -1 }]}
-                            colSpan={3}
-                            count={filteredData.length}
-                            rowsPerPage={rowsPerPage}
-                            page={page}
-                            SelectProps={{
-                                inputProps: {
-                                    'aria-label': 'rows per page',
-                                },
-                                native: true,
-                            }}
-                            onPageChange={handleChangePage}
-                            onRowsPerPageChange={handleChangeRowsPerPage}
-                            ActionsComponent={TablePaginationActions}
-                        />
-                    </TableRow>
-                </TableFooter>
-            </Table>
-        </TableContainer>
-    );
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[5, 10, 20, { label: "All", value: -1 }]}
+              colSpan={3}
+              count={filteredData.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              SelectProps={{
+                inputProps: {
+                  "aria-label": "rows per page"
+                },
+                native: true
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
+  );
 }
